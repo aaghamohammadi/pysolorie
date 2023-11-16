5826fe2c711b2d2b91415fe2ad72c16c96f7e8c7
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2023-11-16 15:16:07 +0330

    Refactor pysolorie.py to model.py
    In this commit, the file pysolorie.py has been refactored and renamed to model.py.
    The import statement in __init__.py has been updated accordingly to reflect this change.

--- src/pysolorie/__init__.py
+++ src/pysolorie/__init__.py
@@ -12,6 +12,6 @@
 #    See the License for the specific language governing permissions and
 #    limitations under the License.
 
-from .pysolorie import HottelModel
+from .model import HottelModel
 
 __all__ = ["HottelModel"]
