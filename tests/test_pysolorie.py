205acf1564664fdcc349ab2d359cb17491da3cb9
Alireza Aghamohammadi <al.aghamohammadi@gmail.com>
2023-12-23 16:50:15 +0330

    Refactor: Move Observer tests to separate file
    - Created a new file tests/test_observer.py.
    - Moved the Observer tests from tests/test_pysolorie.py to tests/test_observer.py.
    - Removed the Observer tests from tests/test_pysolorie.py.

--- tests/test_pysolorie.py
+++ tests/test_pysolorie.py
@@ -14,7 +14,6 @@
 import csv
 import json
 import logging
-import math
 import xml.etree.ElementTree as ET
 from pathlib import Path
 from typing import Any, Dict, List
@@ -27,79 +26,9 @@ import pytest
 from pysolorie import (
     AtmosphericTransmission,
     IrradiationCalculator,
-    Observer,
     Plotter,
     ReportGenerator,
 )
-
-
-@pytest.mark.parametrize(
-    "observer_latitude,"
-    + "observer_longitude,"
-    + "day_of_year,"
-    + "solar_time,"
-    + "expected_zenith_angle",
-    [
-        (
-            35.69,
-            51.39,
-            81,
-            12 * 60 * 60,
-            0.623,
-        ),  # Test at Tehran on the 81st day of the year at noon
-        (
-            35.69,
-            51.39,
-            355,
-            12 * 60 * 60,
-            1.032,
-        ),  # Test at Tehran on the 355th day of the year at noon
-        (
-            3.59,
-            98.67,
-            81,
-            10 * 60 * 60,
-            0.527,
-        ),  # Test at Medan on the 81st day of the year at 10am
-        (
-            64.84,
-            -147.72,
-            1,
-            13 * 60 * 60,
-            1.547,
-        ),  # Test at Fairbanks on the 1st day of the year at 1pm
-        (90, 0, 1, 13 * 60 * 60, 1.972),  # Test at North Pole on January 1st at 1pm
-    ],
-)
-def test_calculate_zenith_angle(
-    observer_latitude: float,
-    observer_longitude: float,
-    day_of_year: int,
-    solar_time: int,
-    expected_zenith_angle: float,
-) -> None:
-    observer: Observer = Observer(observer_latitude, observer_longitude)
-    expected_observer_latitude = math.radians(observer_latitude)
-    expected_observer_longitude = math.radians(observer_longitude)
-
-    assert observer.observer_latitude == pytest.approx(
-        expected_observer_latitude, abs=1e-3
-    )
-    assert observer.observer_longitude == pytest.approx(
-        expected_observer_longitude, abs=1e-3
-    )
-
-    zenith_angle: float = observer.calculate_zenith_angle(day_of_year, solar_time)
-    assert zenith_angle == pytest.approx(expected_zenith_angle, abs=1e-3)
-
-
-def test_calculate_zenith_angle_without_latitude():
-    observer = Observer(None, 0)
-    with pytest.raises(
-        ValueError,
-        match="Missing required data: Observer latitude",
-    ):
-        observer.calculate_zenith_angle(1, 12 * 60 * 60)
 
 
 @pytest.mark.parametrize(
@@ -131,27 +60,6 @@ def test_calculate_transmittance(
         day_of_year, solar_time
     )
     assert pytest.approx(result, abs=1e-3) == expected_transmittance
-
-
-@pytest.mark.parametrize(
-    "day_of_year, expected_sunrise_hour_angle, expected_sunset_hour_angle",
-    [
-        (1, -1.261, 1.261),  # January 1st
-        (81, -math.pi / 2, math.pi / 2),  # March 22nd (equinox)
-        (172, -1.888, 1.888),  # June 21st (solstice)
-    ],
-)
-def test_calculate_sunrise_sunset(
-    day_of_year: int,
-    expected_sunrise_hour_angle: float,
-    expected_sunset_hour_angle: float,
-) -> None:
-    observer: Observer = Observer(observer_latitude=35.69)  # Tehran
-    sunrise_hour_angle, sunset_hour_angle = observer.calculate_sunrise_sunset(
-        day_of_year
-    )
-    assert sunrise_hour_angle == pytest.approx(expected_sunrise_hour_angle, abs=1e-3)
-    assert sunset_hour_angle == pytest.approx(expected_sunset_hour_angle, abs=1e-3)
 
 
 @pytest.mark.parametrize(
